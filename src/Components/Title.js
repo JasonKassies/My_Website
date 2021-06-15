c40f98a7a4a12c5228ad00232058ce3c349d1ed0
Jason k <jason@kassies.ca>
2021-06-15 12:20:13 -0400

    moving over to react

---
+++ src/Components/Title.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const title = () => {
+    return (
+        <div>
+            <section id="banner" data-video="images/banner">
+				<div class="inner">
+					<h1>Welcome</h1>
+					<p>To my online Portfolio, My name is Jason Kassies and I am a full stack developer with an <br /> Advanced Diploma in Software Engineering from 
+						<a href="https://www.conestogac.on.ca/fulltime/software-engineering-technology/courses?id=22386" target="_blank"> Conestoga College </a> <br />
+					</p>
+					
+					<a href="#one" class="button special scrolly">Get Started</a>
+				</div>
+			</section>
+        </div>
+    );
+}
+
+export default title;
---
+++ src/Containers/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from '../logo.svg';
+import Title from '../Components/Title'
+import './App.css';
+import './font-awesome.min.css'
+
+class App extends React.Component {
+	
+	render() {
+
+
+
+
+
+
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					
+				</header>
+			
+				<div>
+					<Title/>
+				</div>
+
+			</div>
+		);
+	}
+}
+
+export default App;
+
+
+
+/*
+title page
+intro/cover letter
+profile buttons
+topic blocks
+project blocks
+thank you bar
+contact me
+
+*/
